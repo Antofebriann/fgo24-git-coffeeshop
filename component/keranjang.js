b79e879586a9a1e71c29a160627e2b6b520018e8
Antofebriann <rezadio270@gmail.com>
2025-04-25 15:08:57 +0700

    chore: component

---
+++ component/keranjang.js
@@ -0,0 +1,30 @@
+export function tampilkanKeranjang(keranjang) {
+  console.log("\n=== Isi Keranjang ===");
+  if (keranjang.length === 0) {
+      console.log("Keranjang masih kosong.");
+  } else {
+      let total = 0;
+      keranjang.forEach((item, i) => {
+          console.log(`${i + 1}. ${item.name} - Rp${item.harga}`);
+          total += item.harga;
+      });
+      console.log(`Total sementara: Rp${total}`);
+  }
+  console.log("\nKetik 'kembali' untuk ke menu utama.");
+}
+
+export function checkout(keranjang) {
+  console.log("\n=== Checkout ===");
+  if (keranjang.length === 0) {
+      console.log("Keranjang kosong. Tidak ada yang dipesan.");
+  } else {
+      let total = 0;
+      keranjang.forEach((item, i) => {
+          console.log(`${i + 1}. ${item.name} - Rp${item.harga}`);
+          total += item.harga;
+      });
+      console.log(`\nTotal yang harus dibayar: Rp${total}`);
+      console.log("Terima kasih telah memesan!");
+  }
+  process.exit();
+}
